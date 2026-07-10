2fd1ab205ed999361ffc6d59d04575b4a90435e7
hyannu <annurao9560@gmail.com>
2026-07-10 20:12:00 +0530

    "Add C++ program to print numbers from 10 to 0 using while loop"

--- control flow/loops assignment/loops.cpp
+++ control flow/loops assignment/loops.cpp
@@ -37,13 +37,63 @@
 // }
 
 // while loop using
-#include<iostream>
-using namespace std;
-     int main(){
-    int i=100;
-    while(i>=1){
-        cout <<i <<" " <<endl;
-        i=i-1;
-    }
-    return 0;
-}
+// #include<iostream>
+// using namespace std;
+//      int main(){
+//     int i=100;
+//     while(i>=1){
+//         cout <<i <<" " <<endl;
+//         i=i-1;
+//     }
+//     return 0;
+// }
+
+ 
+// QUESTION NUMBER 3.
+// for loops
+// // name 50 bar print krna hai
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     for(int i=1; i<=50; i=i+1){
+//         cout << "My name is Archana Rao" <<endl;
+//     }
+// }
+
+// while loops
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i=1;
+//     while(i<=50){
+//         cout <<"my name is Archana Rao" <<endl;
+//         i=i+1;
+//     }
+//     return 0;
+// }
+
+// question number 4
+// print 10 to 1;
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=10; i>=1; i=i-1){
+//         cout <<" " <<i <<endl;
+//     }
+//     return 0;
+// }
+
+// WHILE LOOPS
+// 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i=10;
+//     while(i>=0){
+//         cout <<" " <<i <<endl;
+//         i=i-1;
+//     }
+//     return 0;
+// }
