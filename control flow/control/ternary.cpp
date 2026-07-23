65f756fc2fbfefc9501f42243459fb5eab215bfd
hyannu <annurao9560@gmail.com>
2026-07-23 14:03:04 +0530

    add ternary operator example to find maximum value"

--- control flow/control/ternary.cpp
+++ control flow/control/ternary.cpp
@@ -1,10 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int age;
-    cout <<"Enter your age: " <<endl;
-    cin>>age; 
-    (age > 18) ? cout <<"can vote" 
-    :cout <<"cannot vote" ;
+    // int age;
+    // cout <<"Enter your age: " <<endl;
+    // cin>>age; 
+    // (age > 18) ? cout <<"can vote" 
+    // :cout <<"cannot vote" ;
+    
+    int x = 10;
+    int y = 20;
+
+    int result = (x > y) ? x  : y;
+    
+    cout <<"Result: " << result << endl;
+
+
     return 0;
 }
